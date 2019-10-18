0319f17858f06cb4f1e236f92fcf68479f951511
Fabian Hernando Leon Ortega <fhleoinv@gmail.com>
2019-10-17 23:26:16 -0500

    layout footer component, setup loader for awesome fonts

--- src/layouts/Footer/index.js
+++ src/layouts/Footer/index.js
@@ -1,2 +1,47 @@
-const Footer = () => <div> I am the footer</div>
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faWhatsapp,
+  faFacebookF,
+  faTwitter,
+  faInstagram
+} from '@fortawesome/free-brands-svg-icons'
+
+import './Footer.scss'
+
+import '@fortawesome/fontawesome-svg-core/styles.css'
+
+const Footer = () => (
+  <footer>
+    <div className="contact-info-panel">
+      <div className="contact-info-panel__column">
+        <span>
+          <FontAwesomeIcon
+            className="contact-info-panel--margin-left"
+            icon={faWhatsapp}
+          />
+          +57 311 5366601
+        </span>
+      </div>
+      <div className="contact-info-panel__column">
+        <span>Síguenos en nuestras redes sociales</span>
+        <FontAwesomeIcon
+          className="contact-info-panel--margin-w"
+          icon={faFacebookF}
+        />
+        <FontAwesomeIcon
+          className="contact-info-panel--margin-w"
+          icon={faTwitter}
+        />
+        <FontAwesomeIcon
+          className="contact-info-panel--margin-w"
+          icon={faInstagram}
+        />
+      </div>
+    </div>
+
+    <div className="copyright-panel">
+      <span>© 2019 Futuagro.</span>
+    </div>
+  </footer>
+)
 export default Footer
--- src/layouts/Meta.js
+++ src/layouts/Meta.js
@@ -6,7 +6,10 @@ const Meta = ({ pageTitle }) => (
     <title>{pageTitle}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
-    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet"></link>
+    <link
+      href="https://fonts.googleapis.com/css?family=Roboto&display=swap"
+      rel="stylesheet"
+    ></link>
   </Head>
 )
 
